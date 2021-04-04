8a25287c2210e361ac8013ba0e945848e23d56ed
Westfield Zhao <zhaowenrui@WinifredZhaoMacBook-Pro.local>
2021-04-04 15:34:34 +0800

    feat(Entity):Add two entities: Response and Request

---
+++ Entity/Administrator.java
@@ -0,0 +1,68 @@
+package Entity;
+
+public class Administrator {
+    private String adminId;
+    private String name;
+    private String password;
+    private String phoneNo;
+    private String email;
+
+    public Administrator(String adminId, String name, String password, String phoneNo, String email) {
+        this.adminId = adminId;
+        this.name = name;
+        this.password = password;
+        this.phoneNo = phoneNo;
+        this.email = email;
+    }
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator{" +
+                "adminId='" + adminId + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNo='" + phoneNo + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
