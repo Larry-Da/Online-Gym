f604451b2ba3c868c684b0db3607a82c7793b9ca
Frank <lvjinghao0126@hotmail.com>
2021-05-31 19:46:03 +0800

    add javadoc

--- src/main/java/org/qmbupt/grp105/Controller/LiveController.java
+++ src/main/java/org/qmbupt/grp105/Controller/LiveController.java
@@ -10,6 +10,11 @@ import org.qmbupt.grp105.backend.model.Session;
 import java.io.IOException;
 import java.util.*;
 
+/**
+ * The controller used for Live Session Operations
+ * @version 5.3
+ * @author Jinghao Lyu
+ */
 public class LiveController implements LiveUtils{
 
     private static LiveController liveController = new LiveController();
@@ -17,6 +22,12 @@ public class LiveController implements LiveUtils{
     public static LiveController getController(){
         return liveController;
     }
+
+    /**
+     * Get Live Sessions' information by Customer's ID
+     * @param cusId Customer's ID
+     * @return the searched result of a Live Session list
+     */
     public ArrayList<LiveSession> getLiveSessionByCusId(String cusId) {
         ArrayList<LiveSession> res = new ArrayList<>();
         try {
@@ -29,6 +40,12 @@ public class LiveController implements LiveUtils{
         }
         return res;
     }
+
+    /**
+     * Get Live Sessions' information by Coach's ID
+     * @param coId Coach's ID
+     * @return the searched result of a Live Session list
+     */
     public ArrayList<LiveSession> getLiveSessionByCoachId(String coId)
     {
         ArrayList<LiveSession> sessions = getAllLiveSessions();
@@ -42,6 +59,11 @@ public class LiveController implements LiveUtils{
         }
         return res;
     }
+
+    /**
+     * Get all the live Sessions' information without any filters
+     * @return the list of all the live session data
+     */
     public ArrayList<LiveSession> getAllLiveSessions() {
         ArrayList<Session> sessions = null;
         ArrayList<LiveSession> res = new ArrayList<>();
@@ -55,6 +77,12 @@ public class LiveController implements LiveUtils{
         }
         return res;
     }
+
+    /**
+     * Get a specified Live Session's information of a particular id
+     * @param liveSessionId Live Session's ID
+     * @return the searched result of the Live Session
+     */
     public LiveSession getLiveSessionBySessionId(String liveSessionId) {
         LiveSession liveSession = null;
         try {
@@ -64,6 +92,12 @@ public class LiveController implements LiveUtils{
         }
         return liveSession;
     }
+
+    /**
+     * Get Live Sessions' information by Coach's name
+     * @param keyword the keyword related to coaches' names
+     * @return the searched result of a Live Session list
+     */
     public ArrayList<LiveSession> getSessionsByCoach(String keyword) {
         ArrayList<LiveSession> liveSessions = getAllLiveSessions();
         return liveSessions;
@@ -75,6 +109,13 @@ public class LiveController implements LiveUtils{
 //        }
 //        return res;
     }
+
+    /**
+     * Filter a list of Live Sessions by judging whether the live session has been expired or not
+     * @param sessions the list of live sessions needed for filtration
+     * @param expired true for keeping expired live session, false for keeping live session not expired
+     * @return the filtered result of a (expired/unexpired) Live Session list
+     */
     public ArrayList<LiveSession> filterSessionByExpire(ArrayList<LiveSession> sessions, boolean expired) {
         ArrayList<LiveSession> res = new ArrayList<>();
         if(expired)
@@ -102,6 +143,12 @@ public class LiveController implements LiveUtils{
         return res;
     }
 
+    /**
+     * Get Live Sessions' information by a list of category's name
+     * @param sessions the list of live sessions needed for filtration
+     * @param category the list of category's name that works as a filter key
+     * @return the filtered result of a Live Session list
+     */
     public ArrayList<LiveSession> filterSessionByCategory(ArrayList<LiveSession> sessions, List<String> category) {
         ArrayList<LiveSession> sessions1 = null;
         ArrayList<LiveSession> res = new ArrayList<>();
@@ -122,6 +169,10 @@ public class LiveController implements LiveUtils{
         return res;
     }
 
+    /**
+     * Call the function in the SessionManger class to add a new live session with auto-generated session ID
+     * @param liveSession the live session information
+     */
     public void addSession(LiveSession liveSession) {
         try {
             SessionManager.addSession(liveSession.converter());
